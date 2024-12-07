fe7b24a59347b682cce7117686242cf7538f8a93
tomas-vasquez <tomasdetloging@gmail.com>
2024-12-07 06:14:30 -0400

    added basic structure

---
+++ script.js
@@ -0,0 +1,5 @@
+// Función para mostrar una alerta cuando se hace clic en el botón
+document.getElementById("alertButton").addEventListener("click", () => {
+  alert("¡Hola! Has hecho clic en el botón.");
+});
+
